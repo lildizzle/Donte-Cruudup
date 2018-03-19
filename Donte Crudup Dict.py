0c6010a9d0530d93c0ed38164c78a3388fbd0d97
Donte Crudup <donte.crudup@stu.jefferson.kyschools.us>
2018-03-19 08:12:20 -0400

    this is about food and stuff

---
+++ Donte Crudup Dict.py
@@ -0,0 +1,15 @@
+#Donte R. Crudup
+
+Foods = {
+    "HamBurger":"It is a round piece of ground beef  that is put to fry or probably grilled. then it's put on a bun with a various amount of condiments.",
+    "Steak":"A high quality of meat that is cut into thick piece that is usually put to be broiled or fried.",
+    "Filet":"a cut of meat or fish that is boneless and csn be cooked in any type of style.",
+    "Boston butt":"It is a piece of pork that come from the front leg or the shoulder of a pig.",
+    "Meatballs":"Ground meat that is rolled and then has seasoning (sometimes) and is cooked in various ways. lastly top with sauce." ,
+    "Kobe beef":"this meat is from japan and is  raised from black cattle and is very high-end.",
+    
+}
+
+for key in Foods:
+    print"key:" + str(key)
+    print"value:" + str(Foods[key])
