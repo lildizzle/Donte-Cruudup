9bdd5b6f8da4790d99a6eef559aa757e46a4b5cb
Donte Crudup <donte.crudup@stu.jefferson.kyschools.us>
2018-05-02 08:15:50 -0400

    Hi it's lil dizzle on the code!!!

---
+++ basketball.py
@@ -0,0 +1,55 @@
+#donte crudup
+size_of_ball = 30
+color = "orange"
+title = "Willson and NCAA"
+features = " Black strips and bumpy texture"
+name_of_object = "Basketball"  
+shape = "sphere"
+
+
+
+def basketball_color(color):
+    if color is "orange":
+        return True
+    else:
+        input ("what is the color of the basketball? ")
+
+def basketball_shape(shape):
+    if shape is "sphere":
+        input ("What sport is it used in? ")
+    else:
+        input ("what shape is it then? ")
+        print ("this is from a different sport.")
+
+def basketball_title(title):
+    if title is "Willson and NCAA" or "wilson and ncaa":
+        input ("wha
+    else:
+        input ("What does it say on there? ")
+        print ("That is from a different company.")
+
+
+
+def basektball_features(features):
+    if features is ("Black strips and bumpy texture"):
+        print  ("Yup it's a basketball.")
+    else:
+        input ("What are the features? ")
+        print ("This is from a different sport.")
+
+def basketball_name(name_of_object):
+    if name_of_object is "Basketball" or "basketball":
+        return True
+    else:
+        input ("What is it called? ")
+        print ("That is from a different sport")
+
+
+
+basketball_color("orange")
+basketball_shape("basketball") or ("Basketball")
+basketball_title("Wilson and NCAA.") or ("wilson and ncaa.")
+basketball_features("Black strips and bumpy texture")
+basketball_name("Basketball") or ("basketball")
+
+
